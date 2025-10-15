5cc2c7f98bd0dda4cbefa97edfdfc1d9de4bf911
Yuvraj Purane <yuvrajpurane00@gmail.com>
2025-10-15 23:41:23 +0530

    Update and rename Jenkinsfile to CMD.sh

---
+++ CMD.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Simple bash script to execute Linux commands
+
+echo "Executing command no.1"
+date
+
+echo "Executing command no.2"
+whoami
+
+echo "Executing command no.3"
+ls -l
+
+echo "Executing command no.4"
+df -h
+
+echo "Executing command no.5"
+echo "Result of 22/7 using bc:"
+echo "22/7" | bc -l
